37ece3e2996b57228e85cf3736d35dca9e4f31e8
Cornel Stoica <cornel.stoica@oit.edu>
2024-10-04 01:12:48 -0700

    set up get and post endpoints

--- api/views.py
+++ api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializer import UserSerializer
 
-# Create your views here.
+
+# Endpoints
+# Get requests
+@api_view(['GET'])
+def get_users(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+# Post requests
+@api_view(['POST'])
+def create_user(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
